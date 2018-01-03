4307e0230c47a0df3ce6244ddd6d8d56d2eba533
samChen <293599505@qq.com>
2018-01-03 11:04:29 +0800

    vuedomes

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/Hi">Hi</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
---
+++ my-project/src/components/hi.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-if='isLogin'>hello Vue</div>
+    <div v-else>bye Vue</div>
+    <div v-show='isLogin'>哈哈哈</div>
+    <span v-bind:title="msg">
+    鼠标悬停几秒钟查看此处动态绑定的提示信息！
+  </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'hi Vue'+ new Date().toLocaleString(),
+      isLogin:true,
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
